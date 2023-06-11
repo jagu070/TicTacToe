45843e43336f34c927ae302617fbaaf59a173441
Jagadish s <sjagadish7165@gmail.com>
2023-06-11 12:10:36 +0530

    Initial Commit

---
+++ Shared/TickityTacTowApp.swift
@@ -0,0 +1,15 @@
+//
+//  TickityTacTowApp.swift
+//  
+//
+
+import SwiftUI
+
+@main
+struct TickityTacTowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
